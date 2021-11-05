e6c0e697d4571970461571ca7b9a66a84c6e75c1
SamHdb <sboucetta@hotmail.fr>
2021-11-05 01:24:27 +0100

    commit 05112021

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -10,7 +10,7 @@ public class AnalyticsCounter {
 
 		/* on lit le fichier qui liste les effets secondaires recensés */
 
-		ReadSymptomDataFromFile readSymptoms = new ReadSymptomDataFromFile("C:\\Users\\hdb20\\Documents\\Workspace\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+		ReadSymptomDataFromFile readSymptoms = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		List<String> symptoms = readSymptoms.getSymptoms();
 
 		SymptomsProcess symptomsList = new SymptomsProcess();
