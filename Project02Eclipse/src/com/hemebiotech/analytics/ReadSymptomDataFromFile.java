9d15fc3c75c7f0cc2d8280c0d2ea0d3617e4c9c3
SamHdb <sboucetta@hotmail.fr>
2021-11-05 17:26:49 +0100

    commit 05112021

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -8,37 +8,53 @@ import java.util.List;
 
 /**
  * Simple brute force implementation
- *
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private final String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> getSymptoms() {
-		ArrayList<String> result = new ArrayList<>();
+    private final String filepath;
 
-		if (filepath != null)
-			try {
-					BufferedReader reader = new BufferedReader(new FileReader(filepath));
-					String line = reader.readLine();
+    /**
+     * @param filepath a full or partial path to file with symptom strings in it, one per line
+     */
+    public ReadSymptomDataFromFile(String filepath) {
+        this.filepath = filepath;
+    }
 
-					while (line != null) {
-						result.add(line);
-						line = reader.readLine();
-					}
-					reader.close();
+    @Override
+    public List<String> getSymptoms() {
+        ArrayList<String> result = new ArrayList<>();
+
+        BufferedReader reader = null;
+        FileReader fileReader = null;
+
+        if (filepath != null)
+            try {
+                fileReader = new FileReader(filepath);
+                reader = new BufferedReader(fileReader);
+                String line = reader.readLine();
+
+                while (line != null) {
+                    result.add(line);
+                    line = reader.readLine();
+                }
 			} catch (IOException e) {
-					e.printStackTrace();
-				}
-		return result;
-	}
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fileReader != null) {
+                    try {
+                        fileReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        return result;
+    }
 }
